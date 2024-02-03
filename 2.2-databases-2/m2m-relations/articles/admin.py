bbcd3f130b9e93ff089eb08ac2a3317ee9226a03
Evgeniy Kukarskiy <jon.79555@gmail.com>
2024-02-04 00:01:37 +0700

    Задание по ORM-2 завершено

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+
+            if form.cleaned_data.get('is_main') is not None:
+                counter += 1
+
+        if counter > 1:
+            raise ValidationError('Больше одного основного тэга')
+        elif counter == 0:
+            raise ValidationError('Нужен хотя бы один основной тэг')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title']
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,22 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+
+    title = models.CharField(max_length=255, verbose_name='Тэг')
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='articles')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='articles')
+    is_main = models.BooleanField(default=False)
