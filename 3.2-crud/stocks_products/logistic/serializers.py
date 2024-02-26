6c44fe14eaf6a6b73d1e2d4b1765c75ce72ae27a
Evgeniy Kukarskiy <jon.79555@gmail.com>
2024-02-26 14:56:50 +0700

    Задание выполнено

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,30 @@
 from rest_framework import serializers
 
+from logistic.models import Product, Stock, StockProduct
+
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -26,7 +36,9 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            StockProduct(stock=stock, product=position['product'],
+                         quantity=position['quantity'], price=position['price']).save()
         return stock
 
     def update(self, instance, validated_data):
@@ -40,4 +52,10 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
 
+        for position in positions:
+            stock_prod = StockProduct.objects.get(stock_id=stock.id, product_id=position['product'])
+            stock_prod.product = position['product']
+            stock_prod.quantity = position['quantity']
+            stock_prod.price = position['price']
+            stock_prod.save()
         return stock
